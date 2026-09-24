2498a6f76b25ba9e0460f0c6d75d91810351dade
Jean Schmitz <8446185+jschm42@users.noreply.github.com>
2026-09-24 09:55:15 +0200

    fix(frontend): define __BUNDLED_DEV__ build constant in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [vue()],
+    define: {
+      __BUNDLED_DEV__: false,
+    },
     build: {
       rollupOptions: {
         output: {
